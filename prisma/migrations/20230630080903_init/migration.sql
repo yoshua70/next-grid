65e3f80729beae9eb2b0a6b95e26b345744c0bd8
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-06-30 14:26:20 +0200

    Initialize prisma and create database models

---
+++ prisma/migrations/20230630080903_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Grid" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "rows" INTEGER NOT NULL,
+    "columns" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
