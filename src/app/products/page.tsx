ffe8404ebc188c3f26ad8fa364d6a75defa9ddb9
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-10 21:42:57 +0200

    Complete product main page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,12 @@
 import ProductCreator from "./components/ProductCreator";
+import ProductList from "./components/ProductList";
 
 export default function Page() {
   return (
     <div className="flex flex-col p-12 gap-8">
       <h1 className="text-4xl font-bold">Products</h1>
       <ProductCreator />
+      <ProductList />
     </div>
   );
 }
