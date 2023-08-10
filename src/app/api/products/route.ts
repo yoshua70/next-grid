ccb9350724e6e909a4057f4c034795543f563008
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-08-10 21:26:38 +0200

    Create api routes for products
    Add method to add a product.
    Add method to fetch the list of products.

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,20 @@
+import { prisma } from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    const {label, price} = await request.json();
+
+    const product = await prisma.product.create({
+        data: {
+            label, price
+        }
+    })
+
+    return NextResponse.json(product)
+}
+
+export async function GET(request: Request) {
+    const products = await prisma.product.findMany();
+
+    return NextResponse.json(products)
+}
